710627c3185044d594dd83c403f03a1152dff27c
Nehaaa3121 <nehachoudhary3198@gmail.com>
2025-02-11 01:41:14 +0530

    Problem 10

---
+++ Problem10.sql
@@ -0,0 +1,7 @@
+/*For all the employees in the Employees table, show the FirstName,
+LastName, Title, and BirthDate. Order the results by BirthDate, so we
+have the oldest employees first*/
+
+Select FirstName. LastName.Title,Birthdate
+from employees
+order by birthdate;
